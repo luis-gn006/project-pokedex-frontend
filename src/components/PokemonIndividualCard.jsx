a77dcf7cfca5569fb7cac9b52a6bf06508110ea8
Enrique <luis-enrique06@outlook.com>
2025-03-15 14:41:15 -0600

    update pokemon individual card

--- src/components/PokemonIndividualCard.jsx
+++ src/components/PokemonIndividualCard.jsx
@@ -3,24 +3,31 @@ import pokeballImg from "../images/pokemon__global-pokeball.png"
 import pokemonWeight from "../images/pokemon__weight.png"
 import pokemonHeight from "../images/pokemon__height.png"
 import pokemonBackground from "../images/pokemon__background.png"
+import { useState } from "react";
 
 function PokemonIndividualCard({ pokemon, isOpen, onClose }) {
   if (!pokemon) {
     return null;
   }
 
+  const [isImage1, setIsImage1] = useState(true);
+
   const typesString = pokemon.types
-  .map(type => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1))
-  .join(", ");
+    .map(type => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1))
+    .join(", ");
 
   const weightInKg = (pokemon.weight / 10).toFixed(1);
-  const heightInM = (pokemon.height / 10).toFixed(1); 
+  const heightInM = (pokemon.height / 10).toFixed(1);
 
   function playSound() {
     const audio = new Audio(pokemon.sound);
     audio.volume = 0.5;
     audio.play();
-}
+  }
+
+  function toggleImage() {
+    setIsImage1(!isImage1);
+  }
 
   return (
     <div
@@ -31,14 +38,14 @@ function PokemonIndividualCard({ pokemon, isOpen, onClose }) {
           <img className='pokemon__card_logo' src={pokeballImg} alt="imagen de pokeball" />
           <h3 className="pokemon__card_number">{`No. ${pokemon.id}`}</h3>
           <h3 className="pokemon__card_name">{`${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}`}</h3>
-
         </div>
         <img
-          src={pokemon.image1}
+          src={isImage1 ? pokemon.image1 : pokemon.image2}
           alt={`imagen de ${pokemon.name}`}
           className="pokemon__card-image"
           onClick={playSound}
         />
+        <button onClick={toggleImage} className="pokemon__card_shiny-button">Shiny</button>
         <img
           src={closeIcon}
           alt="icono de cierre"
@@ -56,12 +63,12 @@ function PokemonIndividualCard({ pokemon, isOpen, onClose }) {
             <p className="pokemon__card-weight">{`${weightInKg} kg`}</p>
           </div>
           <div className="pokemon__height">
-              <img
+            <img
               src={pokemonHeight}
               alt={`imagen de regla`}
               className="pokemon__height-img"
             />
-              <p className="pokemon__card-height">{`${heightInM} m`}</p>
+            <p className="pokemon__card-height">{`${heightInM} m`}</p>
           </div>
         </div>
         <ul className="pokemon__card-stats">
